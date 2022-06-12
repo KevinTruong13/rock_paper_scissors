8ad7615fde66d651343db10ee51e408376c65b88
Kevin Truong <ktruon11@montgomerycollege.edu>
2022-06-12 11:54:12 -0400

    Add computer play function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Simulates a computer player that picks randomly between 'Rock', 'Paper', 'Scissors'
 function computerPlay() {
-    
+    switch (randInt(1, 4)) {    // Generates a random int between 1 to 3, corresponding to rock paper scissors values
+        case 1:
+            return 'Rock';
+            break;
+        case 2:
+            return 'Paper';
+            break;
+        case 3:
+            return 'Scissors';
+            break;
+        default:
+            return 'Error in random generation';
+    }
 }
 
 // Function returns a random number between min (inclusive) and max (exclusive)
