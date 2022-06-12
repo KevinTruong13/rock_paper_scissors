a0119d802fa446364f3ee2b3b5ef5cf110d6fba2
Kevin Truong <ktruon11@montgomerycollege.edu>
2022-06-12 11:47:18 -0400

    Add random integer generator function to script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simulates a computer player that picks randomly between 'Rock', 'Paper', 'Scissors'
+function computerPlay() {
+
+}
+
+// Function returns a random number between min (inclusive) and max (exclusive)
+function randInt(min, max) {
+    return Math.floor(Math.random * (max - min)) - min;
+}
